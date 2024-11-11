ec6b9b30e479a996b34a227e08617e47268d1198
DevPBDias <devpbdias@gmail.com>
2024-11-11 09:56:24 -0300

    setup(frontend): config fonts and add icon remove unnecessary code

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const open_sans = Open_Sans({
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Teste FullStack UOLCS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={open_sans.className}>{children}</body>
+    </html>
+  );
+}
