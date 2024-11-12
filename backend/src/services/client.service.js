a8b7aded096f4331878a69e89b328217e7b9bb4e
DevPBDias <devpbdias@gmail.com>
2024-11-11 22:58:26 -0300

    fix(controller): fix update method in controller file

--- backend/src/services/client.service.js
+++ backend/src/services/client.service.js
@@ -2,23 +2,10 @@ import Client from "../model/Client.js";
 
 const createClient = (body) => Client.create(body);
 const getAllClients = () => Client.findAll();
-const getClientById = (id) => Client.findOne({ where: id });
-
-const updateClient = (id, name, email, phoneNumber, personalID, status) =>
-  Client.update(
-    {
-      name,
-      phoneNumber,
-      email,
-      personalID,
-      status,
-    },
-    { where: id }
-  );
+const getClientById = (id) => Client.findByPk(id);
 
 export default {
   createClient,
   getAllClients,
   getClientById,
-  updateClient,
 };
