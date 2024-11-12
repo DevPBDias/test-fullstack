b6f61a29864d49b6dc430f1f8156ac05c21cdae1
DevPBDias <devpbdias@gmail.com>
2024-11-12 16:07:17 -0300

    fix(front): fix regex for phone and personalID

--- frontend/src/schemas/index.ts
+++ frontend/src/schemas/index.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
+const phoneRegex = /^\([1-9]{2}\) (?:[2-8]|9[0-9])[0-9]{3}\-[0-9]{4}$/;
+
+const personalRegex = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/;
+
 export const formSchema = z.object({
   name: z.string().min(3, "Nome curto, insira mais de 2 caracteres"),
   email: z
@@ -8,16 +12,13 @@ export const formSchema = z.object({
   personalID: z
     .string()
     .refine(
-      (value) => /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/.test(value),
-      "Inserir CPF no formato 123.456.789-00"
+      (value) => personalRegex.test(value),
+      "Inserir CPF no formato xxx.xxx.xx-xx"
     ),
   phoneNumber: z
     .string()
     .refine(
-      (value) =>
-        /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/.test(
-          value
-        ),
+      (value) => phoneRegex.test(value),
       "Inserir telefone no formato (xx) xxxxx-xxxx"
     ),
   status: z.string(),
