fdf9d7012bdd12b209a6137b5eb984411c720aa3
DevPBDias <devpbdias@gmail.com>
2024-11-14 12:14:08 -0300

    fix(backend): change update controller logic and create update service

--- backend/src/services/client.service.js
+++ backend/src/services/client.service.js
@@ -3,9 +3,25 @@ import Client from "../model/Client.js";
 const createClient = (body) => Client.create(body);
 const getAllClients = () => Client.findAll();
 const getClientById = (id) => Client.findByPk(id);
+const updateClient = async (id, body) => {
+  const clientFound = await getClientById(id);
+
+  if (!clientFound) return null;
+
+  clientFound.name = body.name;
+  clientFound.phoneNumber = body.phoneNumber;
+  clientFound.email = body.email;
+  clientFound.personalID = body.personalID;
+  clientFound.status = body.status;
+
+  await clientFound.save();
+
+  return clientFound;
+};
 
 export default {
   createClient,
   getAllClients,
   getClientById,
+  updateClient,
 };
