817d0268fb78fe5529f5e575b7a83d1b85e66461
DevPBDias <devpbdias@gmail.com>
2024-11-14 12:08:25 -0300

    feat(front/tests) install jest and rtl and create tests for components and functions

--- backend/src/controllers/client.controller.js
+++ backend/src/controllers/client.controller.js
@@ -64,22 +64,21 @@ const updateClient = async (req, res) => {
       return res.status(400).send({ message: "Submit at least one field" });
     }
 
-    const clientFound = await clientService.getClientById(id);
+    const clientFound = await clientService.updateClient(id, {
+      name,
+      phoneNumber,
+      email,
+      personalID,
+      status,
+    });
 
     if (!clientFound) {
       return response.status(400).json("Client not found");
     }
 
-    clientFound.name = name;
-    clientFound.phoneNumber = phoneNumber;
-    clientFound.email = email;
-    clientFound.personalID = personalID;
-    clientFound.status = status;
-
-    await clientFound.save();
-
     res.status(200).send({ message: "Updated" });
   } catch (error) {
+    console.log(error);
     res.status(500).send({ message: error.message });
   }
 };
